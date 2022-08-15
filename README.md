ab86cb8ed3e24df1045c18da544963a779d3f4de
ahmad-salman98 <ahmad98alsalman@gmail.com>
2022-08-15 21:54:10 +0300

    table of data

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # management-system
 
+part one:
 this application shows some company's employees data and salary.
+
+Part Two :
+this tables shows a sef of employees data an a company
--- app.js
+++ app.js
@@ -23,14 +23,14 @@ function employee(id, fullName, department, level, img, salary) {
 
 
 const staff = [];
-// Array od objects
+// Array of objects
 staff[0] = new employee(1000, "Ghazi Samer", "Administration", "Senior", "https://blog.hubspot.com/hubfs/employee-retention-rate.jpg")
-staff[1] = new employee(1000, "Lana Ali", "Finance", "Senior", "https://www.capsulink.com/assets/img/features-mdm/api-for-developers.svg")
-staff[2] = new employee(1000, "Tamara Ayoub", "Marketing", "Senior", "https://www.capsulink.com/assets/img/features-mdm/api-for-developers.svg")
-staff[3] = new employee(1000, "ٍSaif Walid ", "Administration", "Mid-Senior", "https://blog.hubspot.com/hubfs/employee-retention-rate.jpg")
-staff[4] = new employee(1000, "Omar Zaid", "Development", "Senior", "https://blog.hubspot.com/hubfs/employee-retention-rate.jpg")
-staff[5] = new employee(1000, "Rana Saleh", "Development", "Junior", "https://www.capsulink.com/assets/img/features-mdm/api-for-developers.svg")
-staff[6] = new employee(1000, "Hadi Ahmad", "Finance", "Mid-Senior", "https://blog.hubspot.com/hubfs/employee-retention-rate.jpg")
+staff[1] = new employee(1001, "Lana Ali", "Finance", "Senior", "https://www.capsulink.com/assets/img/features-mdm/api-for-developers.svg")
+staff[2] = new employee(1002, "Tamara Ayoub", "Marketing", "Senior", "https://www.capsulink.com/assets/img/features-mdm/api-for-developers.svg")
+staff[3] = new employee(1003, "ٍSaif Walid ", "Administration", "Mid-Senior", "https://blog.hubspot.com/hubfs/employee-retention-rate.jpg")
+staff[4] = new employee(1004, "Omar Zaid", "Development", "Senior", "https://blog.hubspot.com/hubfs/employee-retention-rate.jpg")
+staff[5] = new employee(1005, "Rana Saleh", "Development", "Junior", "https://www.capsulink.com/assets/img/features-mdm/api-for-developers.svg")
+staff[6] = new employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior", "https://blog.hubspot.com/hubfs/employee-retention-rate.jpg")
 
 
 function employeesData(obj) {
@@ -40,5 +40,63 @@ function employeesData(obj) {
     }
 }
 employeesData(staff);
+
+// _______________ DOM AND ADD ELEMENTS ____________________
+
+
+
+// ____________ Table construction ____________
+
+const main = document.querySelector("main");
+const table = document.createElement("table");
+main.appendChild(table);
+const tableheadrow = document.createElement("tr");
+table.appendChild(tableheadrow);
+
+
+const employeeID = document.createElement("th");
+const fullName = document.createElement("th");
+const department = document.createElement("th");
+const level = document.createElement("th");
+const salary = document.createElement("th");
+
+employeeID.append("Employee ID");
+fullName.append("Full Name");
+department.append("Department");
+level.append("Level");
+salary.append("salary");
+
+tableheadrow.append(employeeID, fullName, department, level, salary);
+
+function addrows(array) {
+
+
+
+    const table = document.querySelector("main table")
+
+    for (let i = 0; i < array.length; i++) {
+
+        let row = table.insertRow(i + 1);
+        let id = row.insertCell(0)
+        id.innerHTML = (array[i].id);
+
+        let name = row.insertCell(1);
+        name.innerHTML = (array[i].fullName);
+
+        let department = row.insertCell(2);
+        department.innerHTML = (array[i].department);
+
+        let level = row.insertCell(3);
+        level.innerHTML = (array[i].level);
+
+        let salary = row.insertCell(4);
+        salary.innerHTML = (array[i].salary())
+    }
+
+}
+addrows(staff);
+
+
+
 
 
